5e7475abd96971db2a439710b4390e330f1caa04
ppanay <ppanayotes@gmx.com>
2023-10-26 15:40:34 +0300

    ..

--- ga.c
+++ ga.c
@@ -1,14 +1,12 @@
 /* ga.c */
 
-#include "z_err.h"
-#include <assert.h>
 #include <stdio.h>
-
 #include "ga.h"
 #include "ga_a.h"
 #include "ga_crossover.h"
 #include "ga_nextg.h"
 #include "ga_types.h"
+#include "z_err.h"
 
 static int ready = 0;
 static Population oldgeneration, newgeneration;
@@ -17,6 +15,7 @@ static int Roulettenum, Beststay;
 static int best[200], score[256];
 static float Mutp, Mutdist, Base;
 int genomenum, dnanum;
+
 
 void ga_init(int dnan, int genomen) {
   int i;
@@ -34,6 +33,7 @@ void ga_init(int dnan, int genomen) {
   ready = 1;
 }
 
+
 void ga_params(int roulettenum, int beststay, float mutdist, float mutp,
                float base) {
   check(ready);
@@ -42,20 +42,18 @@ void ga_params(int roulettenum, int beststay, float mutdist, float mutp,
   check(mutdist < 200);
   check(mutp >= 0 && mutp <= 1);
   check(base >= 0 && base <= 1);
-
   Roulettenum = roulettenum;
   Beststay = beststay;
   Mutdist = mutdist;
   Mutp = mutp;
   Base = base;
-
   ready = 2;
 }
+
 
 void ga_run(long gens, int (*costfunction)(Genome)) {
   long i;
   Population *tmp;
-
   check(ready > 1);
   for (i = 0; i < gens; i++) {
     tmp = old;
@@ -65,6 +63,7 @@ void ga_run(long gens, int (*costfunction)(Genome)) {
             score);
   }
 }
+
 
 void ga_printbests(int m) {
   int i;
