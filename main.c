84381f36bff5480ef6f87ddc41598b20e13ec65e
ppanay <ppanayotes@gmx.com>
2023-11-11 16:50:25 +0200

    ..

--- main.c
+++ main.c
@@ -17,13 +17,13 @@ int main(int n, char **arg) {
   if (n > 1)
     g = atol(arg[1]);
   rndseed();
-  printf(">>> g=%ld  ", g);
 
   ga_init(100, 50);
   ga_params(1000, 5, 2, .1, .01);
   ga_run(g, costfunction1);
-  ga_printbests(1);
   a = bestgenomesofar();
   terminal_draw(a);
+  printf(">>> g=%ld  ", g);
+  ga_printbests(1);
   return 0;
 }
